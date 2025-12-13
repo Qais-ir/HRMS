20e015675c4de46f848c0be8bf7830b73297487b
Qais-ir <qais.irshaid77@gmail.com>
2025-12-13 13:14:23 +0300

    Pipes and Data binding

--- Frontend/HRMS/src/app/app.component.ts
+++ Frontend/HRMS/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf, NgFor, NgClass, NgStyle } from '@angular/common';
+import { NgIf, NgFor, NgClass, NgStyle, CommonModule } from '@angular/common';
 import { RandomColorDirective } from './directives/random-color.directive';
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReversePipe } from './pipes/reverse.pipe';
 // Decorator
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle, RandomColorDirective],
+  imports: [RouterOutlet, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    CommonModule,
+    ReversePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,9 +19,10 @@ export class AppComponent {
  
 
   title = "Welcome to Angular From Typescript";
-  number = 52.555;
+  price = 5212315.555;
   boolean = true;
   arr = ["one", "two", "three"];
+  date = new Date();
 
   students = [
     {id : 0, name : "stu1", mark: 49},
@@ -24,6 +31,67 @@ export class AppComponent {
     {id : 3, name : "stu4", mark: 32},
     {id : 4, name : "stu5", mark: 95},
   ];
+
+  images = [
+    "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70",
+    "https://t3.ftcdn.net/jpg/02/70/35/00/360_F_270350073_WO6yQAdptEnAhYKM5GuA9035wbRnVJSr.jpg",
+    "https://images.pexels.com/photos/26151151/pexels-photo-26151151/free-photo-of-night-sky-filled-with-stars-reflecting-in-the-lake.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  ];
+
+  currentIndex : number = 0; // Global Variable
+
+  name : string = "Qais";
+  age : number = 20;
+
+  infoForm = new FormGroup({
+    name : new FormControl("Qais"),
+    age : new FormControl(25)
+  });
+
+  // Invalid --> Valid
+  form = new FormGroup({
+    email : new FormControl(null, [Validators.required, Validators.email]),
+    name : new FormControl(null, Validators.required),
+    phone : new FormControl(null, [Validators.required, Validators.minLength(9), Validators.maxLength(10)]),
+    age : new FormControl(),
+    course : new FormControl(1, Validators.required)
+  });
+
+  courseOptions = [
+    {id : 1, name : "Angular"},
+    {id : 2, name : "Asp.Net"},
+    {id : 3, name : "Python"},
+    {id : 4, name : "Jave"},
+  ]
+
+  nextImage(){
+    //let temp = 10; // Local Variable
+    if(this.currentIndex < this.images.length - 1){
+      this.currentIndex++;
+    }
+  }
+
+  previousImage(){
+    if(this.currentIndex > 0){
+        this.currentIndex--;
+    }
+
+    //temp = 5;
+  }
+
+  formReset(){
+    this.form.reset({
+      course : 1
+    });
+  }
+
+  submit(){
+    let name = this.form.value?.name;
+    let course = this.form.value?.course; // Id
+    let courseName = this.courseOptions.find(x => x.id == course)?.name;
+    alert(`Welcom ${name} to the academy.
+      We will contact you shortly about the ${courseName} Course.`);
+  }
 
   /*
   test(x : number, y : string) : string{
