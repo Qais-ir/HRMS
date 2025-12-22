9a5ab10fe7fee3451bdf98a757839d135bddab05
Qais-ir <qais.irshaid77@gmail.com>
2025-12-22 21:55:30 +0300

    update and delete employees

--- Frontend/HRMS/src/app/components/employees/employees.component.ts
+++ Frontend/HRMS/src/app/components/employees/employees.component.ts
@@ -3,13 +3,19 @@ import { Employee } from '../../interfaces/employee';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-employees',
   imports: [CommonModule, ReactiveFormsModule, NgxPaginationModule],
+  providers: [DatePipe],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css'
 })
 export class EmployeesComponent {
+
+  constructor(private _datePipe : DatePipe){
+
+  }
 
   @ViewChild ('closeDialog') closeDialog : ElementRef | undefined;
 
@@ -115,6 +121,7 @@ export class EmployeesComponent {
   ];
 
   employeeForm : FormGroup = new FormGroup({
+    id: new FormControl(null),
     name : new FormControl(null, [Validators.required]),
     birthdate : new FormControl(null),
     email : new FormControl(null),
@@ -123,7 +130,7 @@ export class EmployeesComponent {
     positionId : new FormControl(null, [Validators.required]),
     departmentId : new FormControl(null, [Validators.required]),
     managerId : new FormControl(null),
-  })
+  });
 
   employeesTableColumns : string[] = [
     "#",
@@ -156,29 +163,51 @@ export class EmployeesComponent {
     {id : 2, name : "Emp 2"}
   ];
 
-  constructor(){
-  }
 
 
   saveEmployee(){
 
-    let newEmp : Employee = {
-      id: (this.employees[this.employees.length - 1]?.id ?? 0) + 1,
-      name: this.employeeForm.value.name,
-      email: this.employeeForm.value.email,
-      birthdate: this.employeeForm.value.birthdate,
-      salary: this.employeeForm.value.salary,
-      status: this.employeeForm.value.status,
-      userId: (this.employees[this.employees.length - 1]?.userId ?? 0) + 1,
-      departmentId: this.employeeForm.value.departmentId,
-      departmentName: this.departments.find(x => x.id == this.employeeForm.value.departmentId)?.name,
-      managerId: this.employeeForm.value.managerId,
-      managerName: this.managers.find(x => x.id == this.employeeForm.value.managerId)?.name,
-      positionId: this.employeeForm.value.positionId,
-      positionName: this.positions.find(x => x.id == this.employeeForm.value.positionId)?.name,
-    };
+    if(!this.employeeForm.value.id){ // Add Employee
 
-    this.employees.push(newEmp);
+        let newEmp : Employee = {
+          id: (this.employees[this.employees.length - 1]?.id ?? 0) + 1,
+          name: this.employeeForm.value.name,
+          email: this.employeeForm.value.email,
+          birthdate: this.employeeForm.value.birthdate,
+          salary: this.employeeForm.value.salary,
+          status: this.employeeForm.value.status,
+          userId: (this.employees[this.employees.length - 1]?.userId ?? 0) + 1,
+          departmentId: this.employeeForm.value.departmentId,
+          departmentName: this.departments.find(x => x.id == this.employeeForm.value.departmentId)?.name,
+          managerId: this.employeeForm.value.managerId,
+          managerName: this.managers.find(x => x.id == this.employeeForm.value.managerId)?.name,
+          positionId: this.employeeForm.value.positionId,
+          positionName: this.positions.find(x => x.id == this.employeeForm.value.positionId)?.name,
+        };
+
+        this.employees.push(newEmp);
+    }
+    else{ // Edit Employee
+
+      let index = this.employees.findIndex(x => x.id == this.employeeForm.value.id); // Returns The Index.
+
+      this.employees[index].name = this.employeeForm.value.name;
+      this.employees[index].birthdate = this.employeeForm.value.birthdate;
+      this.employees[index].email = this.employeeForm.value.email;
+      this.employees[index].salary = this.employeeForm.value.salary;
+      this.employees[index].status = this.employeeForm.value.status;
+      
+      this.employees[index].positionId = this.employeeForm.value.positionId;
+      this.employees[index].positionName = this.positions.find(x => x.id == this.employeeForm.value.positionId)?.name;
+
+      this.employees[index].departmentId = this.employeeForm.value.departmentId;
+      this.employees[index].departmentName = this.departments.find(x => x.id == this.employeeForm.value.departmentId)?.name;
+
+      this.employees[index].managerId = this.employeeForm.value.managerId;
+      this.employees[index].managerName = this.managers.find(x => x.id == this.employeeForm.value.managerId)?.name;
+
+    }
+  
     this.closeDialog?.nativeElement.click();
    // document.getElementById('closrDialog')?.click();
   }
@@ -191,6 +220,47 @@ export class EmployeesComponent {
 
   changePage(pageNumber : number){
     this.paginationConfig.currentPage = pageNumber;
+  }
+
+  loadEmployeeForm(id : number | undefined){
+
+    if(!id){
+      return;
+    }
+
+    let employee = this.employees.find(x => x.id == id);
+
+
+    // null == undeifned  --> true
+    // null === undefined --> false
+
+    // undefined != null --> false
+    // undefined !== null --> true
+    if(employee != null){
+
+      this.employeeForm.patchValue({
+        id : employee.id,
+        name : employee.name,
+        email : employee.email,
+        birthdate : this._datePipe.transform(employee.birthdate, 'yyyy-MM-dd'),
+        salary : employee.salary,
+        status : employee.status,
+        positionId : employee.positionId,
+        departmentId : employee.departmentId,
+        managerId : employee.managerId
+      })
+    }
+  }
+
+
+  removeEmployee(id : number | undefined){
+    if(!id){
+      return;
+    }
+   // this.employees = this.employees.filter(x => x.id != id); // Returns a new array without the deleted id
+
+   let index = this.employees.findIndex(x => x.id == id); // Return Employee Index
+    this.employees.splice(index, 1); // Delete the employee with splice || where start from index and finish after just one index
   }
 }
 
